b83c750e8f90e5e215230ffc55666cbd030230fd
ayoubsmayen <ayoubjobs.2019@gmail.com>
2022-02-17 15:40:44 +0100

    gol

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 package main
-
+/*
 import (
   "net/http"
   "github.com/gin-gonic/gin"
@@ -11,4 +11,34 @@ func main() {
     context.JSON(http.StatusOK, gin.H{"data": "Hello World !"})
   }) // first endpoint returns Hello World
   router.Run(":8000") //running application, Default port is 8080
+}
+*/
+
+package main
+
+import (
+    "github.com/famousyub/securesocial/api/controller"
+    "github.com/famousyub/securesocial/api/repository"
+    "github.com/famousyub/securesocial/api/routes"
+    "github.com/famousyub/securesocial/api/service"
+    "github.com/famousyub/securesocial/infrastructure"
+    "github.com/famousyub/securesocial/models"
+)
+
+func init() {
+    infrastructure.LoadEnv()
+}
+
+func main() {
+
+    router := infrastructure.NewGinRouter() //router has been initialized and configured
+    db := infrastructure.NewDatabase() // databse has been initialized and configured
+    postRepository := repository.NewPostRepository(db) // repository are being setup
+    postService := service.NewPostService(postRepository) // service are being setup
+    postController := controller.NewPostController(postService) // controller are being set up
+    postRoute := routes.NewPostRoute(postController, router) // post routes are initialized
+    postRoute.Setup() // post routes are being setup
+
+    db.DB.AutoMigrate(&models.Post{}) // migrating Post model to datbase table
+    router.Gin.Run(":8000") //server started on 8000 port
 }
